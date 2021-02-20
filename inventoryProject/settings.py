e13325a0fa48a67ae15ab2fc12a77c6bb18ab1ac
Paolo <paolo.fuentes@obf.ateneo.edu>
2021-02-20 23:44:26 +0800

    connected to mysql db on phpmyadmin

--- inventoryProject/settings.py
+++ inventoryProject/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'inventoryProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cordero_system',
+        'USER': 'root',                     
+        'PASSWORD': '',                                  
+        'PORT': '3306',
+        'HOST': 'localhost'
     }
 }
 
