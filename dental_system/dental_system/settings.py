c6b5f78b42878ee2636f61ff7e0a73da55632a95
Paolo <paolo.fuentes@obf.ateneo.edu>
2021-05-30 00:26:16 +0800

    deploy test

--- dental_system/dental_system/settings.py
+++ dental_system/dental_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3srr70ud&&w!)t__rh6b5p0q&##uk!wvxex8jrgdqd90y*t6=t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
