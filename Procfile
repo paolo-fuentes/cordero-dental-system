4dd74e470f246d16dd01a606114cfbaf300683c2
Paolo <paolo.fuentes@obf.ateneo.edu>
2021-05-30 01:14:40 +0800

    deploy test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dental_system.wsgi
---
+++ dentalapp/filters.py
@@ -0,0 +1,46 @@
+import django_filters
+
+from .models import *
+
+class SupplierFilter(django_filters.FilterSet):
+    class Meta:
+        model = Supplier
+        fields = {
+            'contact_person': ['icontains'],
+        }
+
+class CustomerFilter(django_filters.FilterSet):
+    class Meta:
+        model = Customer
+        fields = {
+            'customer_name': ['icontains'],
+        }
+
+class MaterialFilter(django_filters.FilterSet):
+    class Meta:
+        model = Material
+        fields = {
+            'material_name': ['icontains'],
+        }
+
+class DeliveryFilter(django_filters.FilterSet):
+    class Meta:
+        model = Delivered_Material
+        fields = {
+            'parcel_number': ['icontains'],
+        }
+
+class ProcedureFilter(django_filters.FilterSet):
+    class Meta:
+        model = Procedure
+        fields = {
+            'procedure_name': ['icontains'],
+        }
+
+class Required_MaterialFilter(django_filters.FilterSet):
+    class Meta:
+        model = Required_Material
+        fields = {
+            'quantity': ['icontains'],
+        }
+        
