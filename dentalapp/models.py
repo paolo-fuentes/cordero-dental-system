e46f4045b3a95b9c6a8355cede3fa1ddd12153f7
katearnaiz <kmtarnaiz@gmail.com>
2021-05-30 16:40:22 +0800

    added

--- dentalapp/models.py
+++ dentalapp/models.py
@@ -120,7 +120,6 @@ class Procedure(models.Model):
     def __str__(self):
         return str(self.procedure_name)
 
-
 class Required_Material(models.Model):
     procedure = models.ForeignKey(Procedure, on_delete = models.SET_NULL, null=True, related_name='procedure_required_material')
     material = models.ForeignKey(Material,on_delete=models.SET_NULL,null=True)
@@ -129,6 +128,9 @@ class Required_Material(models.Model):
 
     def __str__(self):
         return str(self.material)
+    
+    def rmsupplydisplay(self):
+        return str(self.quantity) + " " + self.material.threshold_value_unit
 
 class Reservation(models.Model):
     customer = models.ForeignKey(Customer, on_delete = models.SET_NULL, null=True)
