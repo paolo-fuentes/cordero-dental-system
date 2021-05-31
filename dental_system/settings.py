3e0a6cf29c2ce3833932d4b25c42532b12c61907
Paolo <paolo.fuentes@obf.ateneo.edu>
2021-05-31 16:23:25 +0800

    template fix

--- dental_system/settings.py
+++ dental_system/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, "templates\dentalapp")
+            os.path.join(BASE_DIR, "dentalapp/templates/dentalapp")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
